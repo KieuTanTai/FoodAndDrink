ec9fc332ceb0fddf6c6c571f67ccff3be07e2ec9
Thien_Lang <kieutantai2x@gmail.com>
2025-08-21 22:42:57 +0700

    fix error missing ;

--- ProjectShop.Server/Core/Interfaces/IData/IUniqueDAO/IDetailCartDAO.cs
+++ ProjectShop.Server/Core/Interfaces/IData/IUniqueDAO/IDetailCartDAO.cs
@@ -5,6 +5,6 @@
         Task<IEnumerable<TEntity>> GetByCartIdAsync(uint cartId, int? maxGetCount = null);
         Task<IEnumerable<TEntity>> GetByCartIdsAsync(IEnumerable<uint> cartIds, int? maxGetCount = null);
         Task<IEnumerable<TEntity>> GetByProductBarcodeAsync(string barcode, int? maxGetCount = null);
-        Task<IEnumerable<TEntity>> GetByProductBarcodesAsync(IEnumerable<string> barcodes, int? maxGetCount = null)
+        Task<IEnumerable<TEntity>> GetByProductBarcodesAsync(IEnumerable<string> barcodes, int? maxGetCount = null);
     }
 }
--- ProjectShop.Server/Core/Interfaces/IData/IUniqueDAO/IProductLotDAO.cs
+++ ProjectShop.Server/Core/Interfaces/IData/IUniqueDAO/IProductLotDAO.cs
@@ -3,6 +3,6 @@
     public interface IProductLotDAO<TEntity> : IGetDataByDateTimeAsync<TEntity> where TEntity : class
     {
         Task<IEnumerable<TEntity>> GetByProductBarcodeAsync(string barcode, int? maxGetCount = null);
-        Task<IEnumerable<TEntity>> GetByProductBarcodesAsync(IEnumerable<string> barcodes, int? maxGetCount = null)
+        Task<IEnumerable<TEntity>> GetByProductBarcodesAsync(IEnumerable<string> barcodes, int? maxGetCount = null);
     }
 }
